6fa33ced197a64bf9fca29b7019d8c39fbc1fb93
jevjay <askjevjay@gmail.com>
2022-04-29 19:11:12 +0200

    Update module output variables

--- output.tf
+++ output.tf
@@ -1,5 +1,13 @@
 output "lambda_arn" {
+  description = "Amazon Resource Name (ARN)"
   value = [
     for function in aws_lambda_function.lambda : function.arn
+  ]
+}
+
+output "lambda_invoke_arn" {
+  description = "ARN to be used for invoking Lambda Function from API Gateway"
+  value = [
+    for function in aws_lambda_function.lambda : function.invoke_arn
   ]
 }
