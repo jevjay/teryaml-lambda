c53070e53b512fd72f538c85370f15166dfab236
jevjay <askjevjay@gmail.com>
2022-04-29 19:14:49 +0200

    Add dependency to Lambda role Logging policy extension

--- lambda.tf
+++ lambda.tf
@@ -15,6 +15,7 @@ resource "aws_lambda_function" "lambda" {
     variables = each.value.variables
   }
 
+  # Dynamically assign VPC configuration only when such are present
   dynamic "vpc_config" {
     for_each = each.value.vpc
     content {
@@ -23,6 +24,12 @@ resource "aws_lambda_function" "lambda" {
     }
 
   }
+
+  # Logging dependencies
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.lambda,
+  ]
 
   tags = local.tags
 }
